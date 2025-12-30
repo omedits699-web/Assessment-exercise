5652a0bdb86f442eabcbe343b81539544b7c88ea
Umar <omedits699@gmail.com>
2025-12-30 18:42:46 +0300

    Coding Is Cool.py
    Coding Is Cool

--- Exercise_01/Coding Is Cool.py
+++ Exercise_01/Coding Is Cool.py
@@ -2,3 +2,5 @@ word1 = 'coding'                     #variable 1
 word2 = 'is'                        #var2 
 word3 = 'cool'                         #var 3
 print(word1+" "+word2+" "+word3)              # giving output of var1,2,3
+
+
